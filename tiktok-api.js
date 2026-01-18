49486b11cf0eb0bee3e5e7c66180c2b5e5c60c06
OmegaNetwork-source <omega@omeganetwork.co>
2026-01-17 21:26:36 -0500

    Remove username field from getUserInfo - requires user.info.profile scope not user.info.basic

--- tiktok-api.js
+++ tiktok-api.js
@@ -314,7 +314,9 @@ class TikTokAPI {
         }
 
         // TikTok Display API: GET /v2/user/info/ with fields as query params
-        const fields = ['open_id', 'union_id', 'avatar_url', 'display_name', 'username'].join(',');
+        // NOTE: user.info.basic scope only supports: open_id, union_id, avatar_url, display_name
+        // 'username' field requires user.info.profile scope, so we've removed it
+        const fields = ['open_id', 'union_id', 'avatar_url', 'display_name'].join(',');
         // Explicitly construct endpoint without leading slash
         const endpoint = `user/info/?fields=${fields}`;
         console.log('📞 getUserInfo - calling apiRequest with endpoint:', endpoint);
